1bd8f3bb32a2d2a64fc99b997ec517a7dacd0f83
Timur Mazitov <timurmazitov000@gmail.com>
2024-09-21 19:48:15 +0400

    update deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,18 @@ export default ({ mode }) => {
     // Load app-level env vars to node-level env vars.
     process.env = {...process.env, ...loadEnv(mode, process.cwd())};
     return defineConfig({
-			plugins: [vue()],
-			base: process.env.VITE_MODE == 'DEV' ?
-				'/' : "/profile/",
-			resolve: {
-				alias: [
-					{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-					{ find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
-				],
-			},
-			server: {
-				host: "0.0.0.0",
-				port: 5173
-			}
+		plugins: [vue()],
+		base: process.env.VITE_MODE == 'DEV' ?
+			'/' : "/profile/",
+		resolve: {
+			alias: [
+				{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+				{ find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+			],
+		},
+		server: {
+			host: "0.0.0.0",
+			port: 5173
+		}
     });
 }

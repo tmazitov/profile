907114dcc87b4a9924d8d995fc2fba635481b550
Timur Mazitov <timurmazitov000@gmail.com>
2024-09-21 18:19:09 +0400

    remove the comented extra code from home apge

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -45,15 +45,6 @@ import ProjectList from '../components/project/ProjectList.vue';
 import InfoBlock from '../components/InfoBlock.vue';
 import { journeyEvents } from '../info/journey';
 import MainLayout from '../components/layouts/MainLayout.vue';
-// const router = useRouter()
-// const goToPage = (name: string) => router.push({name})
-// const skills:Array<Skill> = [
-// 	new Skill(1, "golang", "https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg"),
-// 	new Skill(2, "javascript", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902"),
-// 	new Skill(3, "typescript", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/2048px-Typescript.svg.png"),
-// 	new Skill(3, "vue", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/768px-Vue.js_Logo_2.svg.png"),
-// ]
-// const openedSkill = ref(0)
 </script>
 
 <style>

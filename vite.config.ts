c2fbc5ede36cc90e41f7184a3b461d62a93831fb
Timur Mazitov <timurmazitov000@gmail.com>
2024-09-21 19:24:17 +0400

    update viteconfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { URL, fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '@assets', replacement: fileURLToPath(new URL('./src/shared/assets', import.meta.url)) },
-    ],
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 5173
-  }
-})
+export default ({ mode }) => {
+    // Load app-level env vars to node-level env vars.
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+			plugins: [vue()],
+			base: process.env.VITE_MODE == 'DEV' ?
+				'/' : "/profile/",
+			resolve: {
+				alias: [
+					{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+					{ find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+				],
+			},
+			server: {
+				host: "0.0.0.0",
+				port: 5173
+			}
+    });
+}

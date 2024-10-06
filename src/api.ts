93d7851051c0edafe545f8db47e846b13548a42b
Timur Mazitov <timurmazitov000@gmail.com>
2024-10-06 19:03:51 +0400

    update api.ts

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 class API {
 	static getArticle(name:string) {
 		// const isDev = import.meta.env.DEV
-		return axios.get(`/assets/articles/${name}`)
+		return axios.get(`/profile/assets/articles/${name}`)
 	}
 }
 

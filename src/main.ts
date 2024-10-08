32baac4fe4a6552509bcb305a971eec491a33b96
Timur Mazitov <timurmazitov000@gmail.com>
2024-10-08 13:02:02 +0400

    update styles for the table on the article page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import './assets/theme.css'
+import './assets/article.css'
 import App from './App.vue'
 import router from './router';
 import PrimeVue from 'primevue/config';
